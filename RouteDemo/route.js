27754b3764897492799e68173c86100c5c135902
Amit Thakkar <vigildbest@gmail.com>
2014-03-26 08:49:46 +0530

    Adding Underscore js. Addubg Edit and Show functionality to APP.

--- RouteDemo/route.js
+++ RouteDemo/route.js
@@ -4,8 +4,8 @@
 'use strict';
 
 var users = [
-    {name: "Amit", age: 25},
-    {name: "Kanika", age: 45}
+    {id: 1, name: "Amit", age: 25},
+    {id: 2, name: "Kanika", age: 45}
 ];
 
 function MainController($scope) {
@@ -14,26 +14,48 @@ function MainController($scope) {
     };
 }
 
-function UserList($scope) {
+function UserListController($scope) {
     $scope.users = [];
     angular.extend($scope.users, users);
+    $scope.remove = function (id) {
+        $scope.users = _.filter($scope.users, function (user) {
+            return user.id != id;
+        });
+    };
 }
 
-function CreateUser($scope, $location) {
-    $scope.user = {};
+function CreateUserController($scope, $location) {
+    $scope.user = {id: users.length + 1};
     $scope.add = function () {
         users.push(angular.copy($scope.user));
-        $location.path("/list");
+        $location.path("/user/list");
     };
     $scope.cancel = function () {
         $scope.user = {};
     };
 }
 
+function ShowUserController($scope, $routeParams) {
+    $scope.user = _.find(users, function (user) {
+        return user.id == $routeParams.id;
+    });
+}
+
+function EditUserController($scope, $routeParams) {
+    $scope.user = angular.copy(_.find(users, function (user) {
+        return user.id == $routeParams.id;
+    }));
+    $scope.update = function () {
+        users[$scope.user.id - 1] = angular.copy($scope.user);
+    };
+}
+
 angular.module('route', ['ngRoute', 'pasvaz.bindonce'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider
-            .when('/list', {templateUrl: 'partials/list.html', controller: UserList})
-            .when('/create', {templateUrl: 'partials/create.html', controller: CreateUser})
-            .otherwise({redirectTo: '/list'});
+            .when('/user/list', {templateUrl: 'partials/list.html', controller: UserListController})
+            .when('/user/create', {templateUrl: 'partials/create.html', controller: CreateUserController})
+            .when('/user/edit/:id', {templateUrl: 'partials/edit.html', controller: EditUserController})
+            .when('/user/show/:id', {templateUrl: 'partials/show.html', controller: ShowUserController})
+            .otherwise({redirectTo: '/user/list'});
     }]);
