89c75de4d5de657c67a0177d380d82eb9a78b9e2
Amit Thakkar <vigildbest@gmail.com>
2014-03-27 15:11:13 +0530

    Correcting versioning line.

--- DirectiveDemo/directiveTest.js
+++ DirectiveDemo/directiveTest.js
@@ -12,8 +12,8 @@ directiveTestApp.directive('myTemplate', function () { // Registering dtTemplate
          * C stand for Class name
          * M stands for Comment
          * A stands for Attribute
-         * Here we are defining directive with all the type, if we don't provide restrict field then default depend on angular version
-         * Angular 1.2.14 have default restrict value A means directive can be use as attribute only.
+         * Here we are defining directive with all the type, if we don't provide restrict field then default value is
+         * 'A' means directive can be use as attribute only.
          * */
         restrict: "ECMA", // directive can be use as attribute, element, class and comment.
         template: "this is directive template test" // DOM element's inner content should be replace with this.
