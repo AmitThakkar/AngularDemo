2b19996d3e2fd5b6cc340880808077eb3afefd5a
Amit Thakkar <vigildbest@gmail.com>
2014-04-10 15:19:52 +0530

    Adding Comments.

--- RouteDemo/route.js
+++ RouteDemo/route.js
@@ -9,12 +9,14 @@ var users = [
     {id: 3, name: "Kanika", age: 45}
 ];
 
+// Master Controller
 function MainController($scope) {
     $scope.mainPage = {
         title: "User Management"
     };
 }
 
+// UserListController, who is responsible for /user/list Url
 function UserListController($scope) {
     $scope.users = users;
     $scope.remove = function (id) {
@@ -25,6 +27,7 @@ function UserListController($scope) {
     };
 }
 
+// CreateUserController, who is responsible for /user/create Url
 function CreateUserController($scope, $location) {
     $scope.user = {id: users.length + 1};
     $scope.add = function () {
@@ -36,15 +39,14 @@ function CreateUserController($scope, $location) {
     };
 }
 
+// ShowUserController, who is responsible for /user/show Url
 function ShowUserController($scope, $routeParams) {
     $scope.user = _.find(users, function (user) {
         return user.id == $routeParams.id;
     });
 }
 
-/*
-*
-* */
+// EditUserController, who is responsible for /user/edit Url
 function EditUserController($scope, $routeParams, $location) {
     var currentUser = _.find(users, function (user) {
         return user.id == $routeParams.id;
@@ -60,15 +62,23 @@ function EditUserController($scope, $routeParams, $location) {
 }
 
 /*
-* Route Mapping: defining route for routeTest module, and as from angular 1.2 version
-* ngRoute is now different module, which we have include explicitly in depended module.
-* */
+ * Route Mapping: defining route for routeTest module, and as from angular 1.2 version
+ * ngRoute is now different module, which we have include explicitly in depended module.
+ * */
+// Defining a module with name routeTest and providing ngRoute as dependency.
 angular.module('routeTest', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
+        // Defining different pages with different URLs
         $routeProvider
+            // If Url is /user/list then list.html will render and UserListController will execute.
             .when('/user/list', {templateUrl: 'partials/list.html', controller: UserListController})
+            // If Url is /user/create then create.html will render and CreateUserController will execute.
             .when('/user/create', {templateUrl: 'partials/create.html', controller: CreateUserController})
+            // If Url is /user/edit/:id then edit.html will render and EditUserController will execute.
             .when('/user/edit/:id', {templateUrl: 'partials/edit.html', controller: EditUserController})
+            // If Url is /user/show/:id then show.html will render and ShowUserController will execute.
             .when('/user/show/:id', {templateUrl: 'partials/show.html', controller: ShowUserController})
+            // TODO Define other URL mapping here.
+            // If no URL match then otherwise will execute and will redirect to /user/list
             .otherwise({redirectTo: '/user/list'});
     }]);
