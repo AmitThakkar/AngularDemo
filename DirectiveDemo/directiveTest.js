ec3380cff5cff05f4269f78829c036eac7adae58
Amit Thakkar <vigildbest@gmail.com>
2014-04-12 09:41:34 +0530

    1. Adding comment for templateUrl options. 2. Correcting Title for Sharing Scope Demo

--- DirectiveDemo/directiveTest.js
+++ DirectiveDemo/directiveTest.js
@@ -18,5 +18,6 @@ directiveTestApp.directive('myTemplate', function () { // Registering myTemplate
         restrict: "ECMA", // directive can be use as attribute, element, class and comment.
         replace: true, // replace indicates that we want to replace whole div or only inner HTML, default value is false.
         template: "<div>this is directive template test</div>" // DOM element's inner content should be replace with this.
+        // If you want to provide template content from HTML file use templateUrl option instead of template
     };
 });
