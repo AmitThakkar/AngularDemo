caf6c239abd7ac03b2f0e7170aab0d495946416e
Amit Thakkar <vigildbest@gmail.com>
2014-05-02 20:21:16 +0530

    Adding solution directive.

---
+++ Solution/solution.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Amit Thakkar on 2/5/14.
+ */
+
+(function Solution() {
+    var selectors = {
+            dummySelector: "div#solution dummy"
+        },
+        eventHandlers = {
+            dummyAction: function () {
+
+            },
+            bind: function () {
+                jQuery(selectors.dummySelector).click(eventHandlers.dummyAction);
+            },
+            init: new function () {
+                jQuery(document).ready(function () {
+                    eventHandlers.constructor();
+                });
+            },
+            constructor: function () {
+                eventHandlers.bind();
+            }
+        };
+})();
