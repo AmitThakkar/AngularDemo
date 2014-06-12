b5db63f74827d2cce7969120564339835ae86dca
Amit Thakkar <vigildbest@gmail.com>
2014-06-13 00:16:56 +0530

    Showing scope visibility with transclude.

--- TranscludeDemo/transclude.js
+++ TranscludeDemo/transclude.js
@@ -8,10 +8,19 @@
  * */
 var transcludeApp = angular.module('transcludeApp', []);
 
+transcludeApp.controller("TranscludeController", function ($scope) {
+    $scope.name = "Controller";
+});
+
 transcludeApp.directive("transcludeTest", function () {
     return {
-        // template to replace Inner HTML of marked Directive.
-        template: "<div>Template HTML! NgTransclude Directive: <span ng-transclude></span></div>",
+        scope: {},
+        link: function (scope) {
+            scope.name = "Directive";
+        },
+        // template to replace Inner HTML of marked Directive. Template will get
+        // scope of directive but HTML into ng-transclude gets scope of Controller.
+        template: "<div>Template HTML with scope of {{name}}! <br/><br/><br/> NgTransclude Directive: <span ng-transclude></span></div>",
         // transclude:true, put INNER HTML of directive into NgTransclude directive's inner HTML of the template.
         // that transclude HTML will get parent scope and template will get scope of directive whatever directive have.
         // Default value is false. If we provide transclude: 'element' then AngularJS move whole directive node
