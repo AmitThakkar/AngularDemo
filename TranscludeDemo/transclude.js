dd971a750e7449752d3edf18941f6a37cdfd4aad
Amit Thakkar <vigildbest@gmail.com>
2014-06-13 10:29:12 +0530

    Adding more comments.

--- TranscludeDemo/transclude.js
+++ TranscludeDemo/transclude.js
@@ -14,17 +14,29 @@ transcludeApp.controller("TranscludeController", function ($scope) {
 
 transcludeApp.directive("transcludeTest", function () {
     return {
+        // Creating isolated scope to understand scope visibility with NgTransclude directive.
         scope: {},
         link: function (scope) {
             scope.name = "Directive";
         },
-        // template to replace Inner HTML of marked Directive. Template will get
-        // scope of directive but HTML into ng-transclude gets scope of Controller.
-        template: "<div>Template HTML with scope of {{name}}! <br/><br/><br/> NgTransclude Directive: <span ng-transclude></span></div>",
-        // transclude:true, put INNER HTML of directive into NgTransclude directive's inner HTML of the template.
-        // that transclude HTML will get parent scope and template will get scope of directive whatever directive have.
-        // Default value is false. If we provide transclude: 'element' then AngularJS move whole directive node
-        // into NgTransclude node.
+        /*
+         * template to replace Inner HTML of marked Directive. Template will get
+         * scope of directive only(because it having isolated scope) but HTML
+         * into ng-transclude gets scope of Controller where it defined.
+         * */
+        template: "<div>Template HTML with scope of {{name}}! <br/><br/><br/> " +
+            "NgTransclude Directive: <span ng-transclude></span></div>",
+        /*
+         * transclude:true, put INNER HTML of directive into NgTransclude directive's
+         * inner HTML of the template. That transclude HTML will get controller level
+         * scope and template will get scope of directive whatever directive have.
+         *
+         * Default value is false(transclude:false, does not move INNER HMTL into
+         * NgTransclude directive.).
+         *
+         * If we provide transclude: 'element' then AngularJS move whole directive node
+         * into NgTransclude node.
+         * */
         transclude: true,
         replace: true
     }
